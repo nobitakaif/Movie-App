bf503917a23e5b19535da5f90d3acde4212d6d95
mohd kaif <mk2818356@gmail.com>
2026-04-25 05:48:16 +0530

    added db

---
+++ packages/db/index.ts
@@ -0,0 +1,14 @@
+
+
+export { prisma } from "./client";
+export * from "./generated/prisma/client";
+
+// prisma.user.create({
+//   data : {
+//     email : "nobita@gmail.com",
+//     name : "email",
+//     username : "nobitakaif",
+//   }
+// }).then(r => {
+//   console.log(r)
+// })
