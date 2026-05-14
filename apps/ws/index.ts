8c8e1fe13568f1c0b2c22a58f01df86af4e3dae6
mohd kaif <mk2818356@gmail.com>
2026-05-14 18:13:54 +0530

    added only admin can control playback, member can only request

--- apps/ws/index.ts
+++ apps/ws/index.ts
@@ -5,9 +5,15 @@ type WebSocketType = {
     token: string,
     userId? : string
     roomId? : string
+    isAdmin? : boolean
 }
 
 const rooms = new Map<string, Set<ServerWebSocket<WebSocketType>>>()
+// how rooms look like 
+// rooms = {
+//    room123 => Set(ws1, ws2, ws3)
+//    roomABC => Set(ws7, ws8, ws9) something like that 
+// }
 
 const server = Bun.serve({
     fetch(req, serve){
@@ -44,12 +50,16 @@ const server = Bun.serve({
                 ws.close()
                 return 
             }
+            let room = rooms.get(roomId)
 
-            if(!rooms.has(roomId)){
-                rooms.set(roomId, new Set())
+            if (!room) {
+                room = new Set()
+                rooms.set(roomId, room)
             }
 
-            rooms.get(roomId)?.add(ws)
+            ws.data.isAdmin = room.size === 0
+
+            room.add(ws)
 
             console.log(
                 `Rooms ${roomId} users :`,
@@ -57,16 +67,83 @@ const server = Bun.serve({
             )
         },
         message(ws, message){
-            if(message == 'ping'){
-                ws.send('pong')
+            // frontend will send 'PLAY' (socket.send('PLAY'))
+            const roomId = ws.data.roomId!
+            const allUser = rooms.get(roomId)
+            if(!allUser){
+                return 
+            }
+
+            let data: any
+            try{
+                data = JSON.parse(message.toString())
+            }catch(e){
+                console.log("Data parsed FAILED! ")
+                return 
+            }
+
+            // for requesting memeber to admin
+            if(data.type === 'PLAY_REQUEST'){
+                for(const client of allUser){
+                    if(client.data.isAdmin){
+                        client.send(JSON.stringify({
+                            type : 'PLAY_REQUEST',
+                            from : ws.data.userId
+                        }))
+                    }
+                }
+                return 
+            }
+
+            if(data.type === 'PLAY'){
+
+                if(!ws.data.isAdmin){
+                    ws.send(JSON.stringify({
+                        type : "ERROR", 
+                        message : "Only admin can control playback!"
+                    }))
+                    return  
+                }
+                
+                for(const client of allUser){
+                    if(client !== ws){
+                        client.send(JSON.stringify({
+                            type : "PLAY",
+                            currentTime : data.currentTime ?? 0
+                        }))
+                    }
+                }
+                return 
             }
         },
-        close(ws){
-            const roomId = ws.data.roomId!
-            if(rooms.has(roomId)){
-                rooms.get(roomId)?.delete(ws)
+        close(ws) {
+            const roomId = ws.data.roomId
+            if (!roomId) return
+
+            const room = rooms.get(roomId)
+            if (!room) return
+
+            room.delete(ws)
+
+            if (room.size === 0) {
+                rooms.delete(roomId)
+                return
             }
-            ws.send("you're disconnected!")
+
+            // if admin left → assign new admin
+            let hasAdmin = false
+
+            for (const client of room) {
+                if (client.data.isAdmin) {
+                    hasAdmin = true
+                    break
+                }
+            }
+
+            if (!hasAdmin) {
+                const next = room.values().next().value
+                if (next) next.data.isAdmin = true
+            }
         }
     }
 })
